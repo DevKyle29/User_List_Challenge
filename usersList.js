d40e8adaae35d5907c59a27ca2eb101d6d66edda
DevKyle29 <kylebrentfry@gmail.com>
2023-01-24 16:37:42 +0200

    in Progress
    pending

---
+++ usersList.js
@@ -0,0 +1,8 @@
+let myInput;
+let myInputBox;
+let myDatabase = ['John','Luke','Joshua','Jude','Katelyn','Lynsay','Gemma','Gabriella','Lyle','Siviwe'];
+
+function choice() {
+    myInput = document.getElementById("theInput");
+    myInputBox = myInputBox.value;
+}

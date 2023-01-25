40cbf0b65ea9ab61faa98528275f3e717bc3e0d4
DevKyle29 <kylebrentfry@gmail.com>
2023-01-25 22:59:17 +0200

    vvv
    vvv

--- usersList.js
+++ usersList.js
@@ -1,6 +1,22 @@
 let database = ['john','Maggie','Andrew','Sifiso','Nwabisa','Bennie','Rafiek','Suzan','Henry','Jack'];
 
-let selection = prompt('Please choose a number');
-console.log(selection);
-
-
+function myFunction(){
+let text;
+let selection = prompt();
+switch(selection) {
+  case '1':
+    text = "Add three Users";
+    break;
+  case "2":
+    text = "Remove User";
+    break;
+  case "3":
+    text = "Display all users";
+    break;
+  case "4":
+    text = "Sort users";
+    
+ }
+ document.getElementById("demo").innerHTML = text;
+}
+ 
